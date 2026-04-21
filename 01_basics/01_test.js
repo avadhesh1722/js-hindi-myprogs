dcd9a5fe6a2db1ccf1dcb095953bb88fad3579ec
Avadhesh Mayur Vora <91149450+avadhesh1722@users.noreply.github.com>
2026-04-21 15:02:27 +0000

    changes done

---
+++ 01_basics/01_test.js
@@ -0,0 +1,9 @@
+console.log("Hitesh");
+
+const accountId = 144553
+let accountEmail = "12345"
+let accountCity = "Jaipur"
+// account = 30045571 not allowed, prefer not to use var - because of scope , ; not necessary
+accountEmail = "30045571"
+
+console.log([accountId, accountEmail, accountCity]);
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = "Avdhesh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) // typecast
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let score1 = null ; let score2 = undefined;
+
+let isLoggedIn = 1 // true -> 1 , false => 0 , "Avdhesh" => true , empty string -> false
+
+let InBooleanvalueofisLoggedIn = Boolean(isLoggedIn)
+console.log(InBooleanvalueofisLoggedIn);
