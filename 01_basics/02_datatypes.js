5ab77528595aa106960d9a43c05f14dd8bdf9ebf
Avadhesh Mayur Vora <91149450+avadhesh1722@users.noreply.github.com>
2026-04-21 12:01:36 +0000

    Datatypes in JavaScript with Andheri's summer nostalgia

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) -> we are using nodejs environment and not browser
+
+let isloggedIn = false
+let state;
+/*
+number => 2 to the power of 53 , bigint for banks stocks
+string => ""/''
+boolean => true/ false
+null - standalone value representing Emptiness
+undefined =>
+symbol => uniqueness
+
+object
+*/
+
+console.log(typeof state) undefined
+console.log(typeof null)  object
